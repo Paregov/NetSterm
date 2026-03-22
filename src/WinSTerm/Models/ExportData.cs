1d056d9e78d35d95a8a1d4bda7141e7b344e4245
Svetoslav Paregov <paregov@gmail.com>
2026-03-22 11:12:39 -0400

    chore: Remove unused ExportData model

--- src/WinSTerm/Models/ExportData.cs
+++
@@ -1,9 +0,0 @@
-namespace WinSTerm.Models;
-
-public class ExportData
-{
-    public string Version { get; set; } = "1.0";
-    public DateTime ExportedAt { get; set; } = DateTime.UtcNow;
-    public List<ConnectionFolder> Folders { get; set; } = new();
-    public List<ConnectionInfo> Connections { get; set; } = new();
-}
